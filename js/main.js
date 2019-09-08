1456b9e6ad76ee54f46bf237b128ed962f85c72d
divineevans <divineevans01@gmail.com>
2019-09-08 16:20:06 +0400

    addition of slider 3

--- js/main.js
+++ js/main.js
@@ -30,9 +30,44 @@ $(".slider-two")
             breakpoint:650,
             settings:{
                 slidesToShow:3,
-                slidesToScroll:3
+                slidesToScroll:3,
             }
         }
         
+    ]
+});
+
+/**third slider */
+$(".slider-three")
+    .not(".slick-initialized")
+    .slick({    
+    prevArrow:".site-slider-three .slider-btn .prev",
+    nextArrow:".site-slider-three .slider-btn .next",
+    slidesToShow:4,
+    slidesToScroll:4,
+    autoplaySpeed:3000,
+    responsive: [
+        {
+            breakpoint:900,
+            settings:{
+                slidesToShow:4,
+                slidesToScroll:2
+            }
+        },
+        {
+            breakpoint:800,
+            settings:{
+                slidesToShow:3,
+                slidesToScroll:2,
+            }
+        },
+        {
+            breakpoint:650,
+            settings:{
+                slidesToShow:2,
+                slidesToScroll:2,
+            }
+        },
+       
     ]
 });
