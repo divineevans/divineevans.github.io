cdbec8141eb9709d5b8b46aea4540005fac28af5
divineevans <divineevans@reenevans.lan>
2019-09-08 13:23:47 +0400

    uploading primary files

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/**first slider */
+$(".slider-one")
+    .not(".slick-initialized")
+    .slick({
+    autoplay:true,
+    autoplaySpeed:3000,
+    dots:true,
+    prevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next"
+});
+
+/**second slider */
+$(".slider-two")
+    .not(".slick-initialized")
+    .slick({  
+    prevArrow:".site-slider-two .slider-btn .prev",
+    nextArrow:".site-slider-two .slider-btn .next",
+    slidesToShow:5,
+    slidesToScroll:1,
+    autoplaySpeed:3000
+});
